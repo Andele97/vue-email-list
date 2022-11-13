3ce6a5cb86d3ee2a4f4c5435434c57cef81c4406
Andele97 <andreapinna40@live.it>
2022-11-13 21:15:10 +0100

    print email

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 const { createApp } = Vue;
 
 createApp({
-    Data() {
-        return {};
+    data() {
+        return {
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            email: [],
+        };
     },
-    methods: {},
-    mounted() {},
-}).mount("#app");
+    methods: {
+        getApi() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.apiUrl)
+                    .then(list => {
+                        this.email.push(list.data.response);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            }
+        },
+    },
+    mounted() {
+        this.getApi();
+    },
+}).mount('#app');
